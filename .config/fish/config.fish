71f22332848d988e59318c3af8c85241f0b51bd1
chazzox <42206116+chazzox@users.noreply.github.com>
2021-09-29 20:21:04 +0100

    Updated with new dot files

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias ls="ls -a"
+alias audio="ncpamixer"
+
+oh-my-posh --init --shell fish --config ~/.poshthemes/aliens.omp.json | source
