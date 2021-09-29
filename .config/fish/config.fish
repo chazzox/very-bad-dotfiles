e87ab62a72e6ab3cdff443b02923b7d17d5c5501
chazzox <42206116+chazzox@users.noreply.github.com>
2021-09-29 20:59:23 +0100

    Updated ls alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias ls="ls -a"
+alias ls="ls -Ga"
 alias audio="ncpamixer"
 
 oh-my-posh --init --shell fish --config ~/.poshthemes/aliens.omp.json | source
