05a238a7c0cc4e1940b17303ec46c1b46f7cca3b
chazzox <42206116+chazzox@users.noreply.github.com>
2022-03-28 19:56:57 +0100

    updated nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,54 +1,57 @@
+-- CHAZZOX NVIM CONFIG
+
 -- editor stuff
-vim.opt.encoding = "UTF-8"
-vim.opt.cursorline = true
-vim.opt.number = true
-vim.opt.mouse = "a"
-vim.opt.scrolloff = 12
+vim.opt.encoding = "UTF-8" vim.opt.cursorline = true vim.opt.number = true
+vim.opt.mouse = "a" vim.opt.scrolloff = 12
 
 -- 4 character width tabs
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4 vim.opt.shiftwidth = 2
+
+-- word wrap
+vim.opt.textwidth = 80 vim.opt.wrapmargin = 2
 
 -- no annoying backup files
-vim.opt.backup = false
-vim.opt.writebackup = false
+vim.opt.backup = false vim.opt.writebackup = false
 
 -- auto check for file updates every time the window is entered/focused
--- future api:
+vim.cmd [[ au WinEnter, TabEnter, FocusGained * checktime ]]
 
--- vim.api.nvim_create_autocmd({"WinEnter", "TabEnter", "FocusGained"}, {pattern = "*", callback = checktime })
-
---[[
-local set_i3_filetype = function() vim.opt.filetype = "i3config"
-vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
-  pattern = "~/.config/i3/config",
-  callback = set_i3_filetype
-})
---]]
-
+-- future api: vim.api.nvim_create_autocmd({"WinEnter", "TabEnter",
+-- "FocusGained"}, {pattern = "*", callback = checktime })
 
 -- spell check
-vim.wo.spell = true
-vim.opt.spelllang = {"nl", "en_us"}
+vim.wo.spell = true vim.opt.spelllang = {"nl", "en_us"}
 
 -- configure title string
-vim.opt.title = true
-vim.opt.titlestring = "%F"
+vim.opt.title = true vim.opt.titlestring = "%F"
 
 -- plugins
-vim.cmd [[packadd packer.nvim]]
+vim.cmd [[packadd packer.nvim]] 
+
 local use = require('packer').use
-require('packer').startup(function()
+require('packer').startup(function() 
   use 'wbthomason/packer.nvim' -- Package manager
+  use 'folke/tokyonight.nvim' -- Theme
+  use 'andweeb/presence.nvim' -- Discord Rich Presence 
 
-  -- LSP stuff
-  use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
-
-  -- theme
-  use 'folke/tokyonight.nvim'
+  -- Languages
+  use 'dag/vim-fish' -- Fish syntax 
+  use 'mboughaba/i3config.vim'-- i3 config support 
+  use 'sheerun/vim-polyglot' -- Syntax highlighting lang pack 
 end)
 
--- theme configuration
-vim.o.termguicolors = true
-vim.g.tokoyonight_style = "storm"
-vim.cmd[[colorscheme tokyonight]]
+-- Theme Configuration
+vim.o.termguicolors = true vim.g.tokoyonight_style = "storm"
+vim.cmd [[colorscheme tokyonight]]
+
+
+-- i3 config detection
+vim.cmd [[ au BufNewFile,BufRead ~/.config/i3/config set filetype=i3config ]]
+
+-- future api:
+--[[
+local set_i3_filetype = function() vim.opt.filetype = "i3config"
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, { pattern =
+    "~/.config/i3/config", callback = set_i3_filetype })
+--]]
+
