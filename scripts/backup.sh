a7d84cd759e88d6ccf010031223f17d5fed19d6e
chazzox <caylott@outlook.com>
2022-11-27 20:33:35 +0000

    feat: added installer and backup scripts

---
+++ scripts/backup.sh
@@ -0,0 +1,11 @@
+echo 'backing up nvim'
+cp ~/.config/nvim/init.lua ./nvim/
+
+echo 'backing up tmux'
+cp ~/.config/tmux/tmux.conf ./tmux/
+
+echo 'backing up alacritty'
+cp ~/.config/alacritty/alacritty.yml ./alacritty
+
+echo 'backing up fish'
+cp ~/.config/fish/config.fish ./fish
---
+++ scripts/installer.sh
@@ -0,0 +1,62 @@
+git submodule init
+git pull --recurse-submodules
+
+echo '
+#################################################
+#         MY LOVELY DOTFILE INSTALLER           #
+#                made by chazzox                #
+#################################################\n\n'
+
+echo '
+#################################################
+#            Please not that before             #
+#             you run this script               #
+#     have fish, tmux and alacrity installed    #
+#################################################'
+
+requirements_valid=true
+
+SUB='fish'
+
+if [[ "$SHELL" =~ .*"$SUB".* ]]; then
+ echo
+else
+  echo
+  echo fish not detected please install via brew or 
+  echo https://fishshell.com/
+  echo
+  requirements_valid=false
+fi
+
+
+if [ $TERM != "alacritty" ]; then
+  echo alacritty not detected please install via brew or 
+  echo https://github.com/alacritty/alacritty
+  echo
+  requirements_valid=false
+fi
+
+if [ "$requirements_valid" = false ]; then
+  echo Something went wrong, exiting
+  exit 0
+else
+  echo "All requirements found"
+fi
+
+
+read -p "Are you sure you want to continue? <y/N> " prompt
+if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
+  echo '\ncreating config directory at ~/.config\n'
+  mkdir -p ~/.config
+  
+  echo '\ncreating tmux config at ~/.config/tmux\n'
+  mkdir -p ~/.config/tmux
+  cp ./tmux/tmux.conf ~/.config/tmux/
+  
+  echo '\ncreating alacritty config at ~/.config/alacritty\n'
+  mkdir -p ~/.config/alacritty
+  cp ./alacritty/alacritty.yml ~/.config/alacritty
+else 
+  echo 'exiting...'
+  exit 0
+fi
